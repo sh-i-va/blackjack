d0b51f25577ea18f636a4dc5ae2f7807bd54ead1
ShivaPrasad <shiva.prasadv@yahoo.com>
2016-05-27 20:26:40 +0530

    Convert the code into object oriented
    The whole source was written in procedural method. Change the code
    to use classes. This is not an atomic change.
    
    The Card struct became a Card class, and all the functions that used
    it now became member functions. Similarly, the Card array deck now
    became the member of a Deck class, and the related functions are wrapped
    up in it. The only additions are the constructors of both classes.
    
    The pointer *cardPtr that pointed to the top most Card in the deck array
    has been scrapped. The member m_cardIndex of the Deck class holds this
    information now. The method dealCard() takes care of dealing the cards
    and incrementing m_cardIndex.
    
    Signed-off: Shiva Prasad shiva.prasadv@yahoo.com

--- source.cpp
+++ source.cpp
@@ -3,61 +3,59 @@
 #include <cstdlib> // for rand() and srand()
 #include <ctime> // for time()
 #include <cctype> // for toupper()
+#include <cassert> // for assert()
 
-// Enumerations for suits and ranks
-enum CardSuit
+// Class to hold a single card, with suit and rank
+class Card
 {
-    SUIT_CLUBS,
-    SUIT_DIAMONDS,
-    SUIT_HEARTS,
-    SUIT_SPADES,
-    MAX_SUITS
+    public:
+        // Enumerations for suits and ranks
+        enum CardSuit
+        {
+            SUIT_CLUBS,
+            SUIT_DIAMONDS,
+            SUIT_HEARTS,
+            SUIT_SPADES,
+            MAX_SUITS
+        };
+
+        enum CardRank
+        {
+            RANK_2,
+            RANK_3,
+            RANK_4,
+            RANK_5,
+            RANK_6,
+            RANK_7,
+            RANK_8,
+            RANK_9,
+            RANK_10,
+            RANK_JACK,
+            RANK_QUEEN,
+            RANK_KING,
+            RANK_ACE,
+            MAX_RANKS
+        };
+
+    private:
+        CardSuit m_suit;
+        CardRank m_rank;
+
+    public:
+        Card(CardRank rank = RANK_ACE, CardSuit suit = SUIT_SPADES): m_rank(rank), m_suit(suit) {}
+
+        void printCard() const; // print the card
+        int getCardValue(int) const; // get the card's value
 };
 
-enum CardRank
-{
-    RANK_2,
-    RANK_3,
-    RANK_4,
-    RANK_5,
-    RANK_6,
-    RANK_7,
-    RANK_8,
-    RANK_9,
-    RANK_10,
-    RANK_JACK,
-    RANK_QUEEN,
-    RANK_KING,
-    RANK_ACE,
-    MAX_RANKS
-};
-
-// Generate a random number between given limits (included)
-int getRandomNumber (int min, int max)
-{
-    // Assumes srand() has already been called
-    // Multiplier to reduce the random number to < 1
-    static const double fraction = 1.0 / (static_cast<double> (RAND_MAX) + 1.0);
-
-    // Calculate the random number, normalize, truncate and return it
-    return static_cast<int> (rand() * fraction * (max - min + 1) + min);
-}
-
-// Struct to store a single card, with suit and rank
-struct Card
-{
-    CardSuit suit;
-    CardRank rank;
-};
-
-// Print a given card as a rank-suit 2-letter code
+// Print the card as a rank-suit 2-letter code
 // e.g. Jack of Spades => JS
-void printCard (const Card &card)
+void Card::printCard() const
 {
     using namespace std;
 
     // Print the letter of the card's rank
-    switch (card.rank)
+    switch (m_rank)
     {
         case RANK_2:    cout << '2'; break;
         case RANK_3:    cout << '3'; break;
@@ -76,9 +74,9 @@ void printCard (const Card &card)
     }
 
     // Print the letter of the card's suit
-    switch (card.suit)
+    switch (m_suit)
     {
-        case SUIT_CLUBS:     cout << 'S'; break;
+        case SUIT_CLUBS:     cout << 'C'; break;
         case SUIT_DIAMONDS:  cout << 'D'; break;
         case SUIT_HEARTS:    cout << 'H'; break;
         case SUIT_SPADES:    cout << 'S'; break;
@@ -86,80 +84,12 @@ void printCard (const Card &card)
     }
 }
 
-// Initialise the deck into the unshuffled state (1C -> AS)
-void initDeck (std::array<Card, 52> &deck)
-{
-    // Index for the card to be initialised
-    int card = 0;
-
-    // Step through the suits
-    for (int suit = 0; suit < MAX_SUITS; ++suit)
-    {
-        // Step through the ranks
-        for (int rank = 0; rank < MAX_RANKS; ++rank)
-        {
-            // Use static_cast to convert int to enum
-            deck[card].rank = static_cast<CardRank> (rank);
-            deck[card].suit = static_cast<CardSuit> (suit);
-            
-            // Move to the next card for the next iteration
-            ++card;
-        }
-    }
-}
-
-// Print a complete deck
-void printDeck (const std::array<Card, 52> &deck)
-{
-    // card is a reference struct to the elements of the struct array deck
-    for (const auto &card : deck)
-    {
-        printCard (card);
-        std::cout << ' ';
-    }
-
-    // Terminate with a newline
-    std::cout << '\n';
-    return;
-}
-
-// Swap two cards, passed as references to change the original values
-void swapCards (Card &a, Card &b)
-{
-    // Create a temporary card to store a;
-    Card temp (a);
-    
-    a = b;
-    b = temp;
-    
-    return;
-}
-
-// Shuffle a deck of cards randomly
-void shuffleDeck (std::array<Card, 52> &deck)
-{
-    // First, we need to loop through the deck
-    for (int card = 0; card < 52; ++card)
-    {
-        // Get a random card
-        int randomCard = getRandomNumber (0, 51);
-        
-        // Swap the current card with the random card
-        swapCards (deck[card], deck[randomCard]);
-    }
-
-    return;
-}
-
-// Calculate and return the value of a card
+// Calculate and return the value of the card
 // currentScore is required to calculate the value of Ace
-int getCardValue (Card &card, int currentScore)
+int Card::getCardValue(int currentScore) const
 {
-    // Set the initial value to 0
-    int value = 0;
-
     // Only rank is relevant here
-    switch (card.rank)
+    switch (m_rank)
     {
         case RANK_2     : return 2;
         case RANK_3     : return 3;
@@ -187,21 +117,136 @@ int getCardValue (Card &card, int currentScore)
     }
 }
 
+// Class to hold a deck of cards
+class Deck
+{
+    private:
+        std::array<Card, 52> m_deck;
+        int m_cardIndex; // stores the index of the current topmost card in the deck
+
+    public:
+        Deck()
+        {
+            initDeck();
+        }
+
+        void initDeck(); // initialise the deck
+        void printDeck(); // print all the cards in the deck
+        void shuffleDeck(); // shuffle the current deck randomly
+        const Card& dealCard(); // deal the topmost card from the deck
+
+    private:
+        // helper functions, don't need acces to *this
+        static int getRandomNumber(int, int);
+        static void swapCards(Card &, Card &);
+};
+
+// Initialise the deck into the unshuffled state (2C -> AS)
+void Deck::initDeck()
+{
+    // Index for the card to be initialised
+    int card = 0;
+
+    // Step through the suits
+    for (int suit = 0; suit < Card::MAX_SUITS; ++suit)
+    {
+        // Step through the ranks
+        for (int rank = 0; rank < Card::MAX_RANKS; ++rank)
+        {
+            // Use static_cast to convert int's to enum's
+            m_deck[card] = Card(static_cast<Card::CardRank>(rank), static_cast<Card::CardSuit>(suit));
+            
+            // Move to the next card for the next iteration
+            ++card;
+        }
+    }
+    
+    // Start dealing from the top
+    m_cardIndex = 0;
+}
+
+// Print the complete deck
+void Deck::printDeck()
+{
+    // card is a reference object to the elements of the member array m_deck
+    for (const auto &card : m_deck)
+    {
+        card.printCard();
+        std::cout << ' ';
+    }
+
+    // Terminate with a newline
+    std::cout << '\n';
+    return;
+}
+
+// Swap two cards, passed as references to change the original values
+void Deck::swapCards(Card &a, Card &b)
+{
+    // Create a temporary card to store a;
+    Card temp(a);
+    
+    a = b;
+    b = temp;
+    
+    return;
+}
+
+// Generate a random number between given limits (included)
+int Deck::getRandomNumber(int min, int max)
+{
+    // Assumes srand() has already been called
+    // Multiplier to reduce the random number to < 1
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+
+    // Calculate the random number, normalize, truncate and return it
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+// Shuffle the deck of cards randomly
+void Deck::shuffleDeck()
+{
+    // First, we need to loop through the deck
+    for (int card = 0; card < 52; ++card)
+    {
+        // Get a random card
+        int randomCard = Deck::getRandomNumber(0, 51);
+        
+        // Swap the current card with the random card
+        swapCards(m_deck[card], m_deck[randomCard]);
+    }
+
+    // After shuffling, we need to start dealing from the top again
+    m_cardIndex = 0;
+
+    return;
+}
+
+// Deal the topmost card from the deck
+const Card& Deck::dealCard()
+{
+    assert (m_cardIndex < 52);
+    
+    // Print the dealt card to the console
+    m_deck[m_cardIndex].printCard();
+
+    // Then return a const reference to the dealt card in the m_deck array
+    // and advance the index to the next card
+    return m_deck[m_cardIndex++];
+}
+
 // Print the current scores of the dealer and the player
-void printScores (int dealerScore, int playerScore)
+void printScores(int dealerScore, int playerScore)
 {
     std::cout << "\n\nCurrent scores\n--------------\n";
     std::cout << "Dealer: " << dealerScore << "\tPlayer: " << playerScore << std::endl;
     return;
 }
 
-// Play a single game of Blackjack
+// Play a single game of Blackjack ('deck' is a shuffled deck)
 // Return true if the player wins, false if they lose
-bool playBlackjack (std::array<Card, 52> &shuffledDeck)
+bool playBlackjack(Deck &deck)
 {
-    // Point to the next card to be dealt/drawn out (initially the first card)
-    Card *cardPtr = &shuffledDeck.at(0);
-
     // Variables to store the players' scores
     int playerScore {0};
     int dealerScore {0};
@@ -211,19 +256,16 @@ bool playBlackjack (std::array<Card, 52> &shuffledDeck)
 
     // Deal one card to the dealer, print it and add it's value to their score
     std::cout << "\nDealer: ";
-    printCard (*cardPtr);
-    dealerScore += getCardValue (*cardPtr++, dealerScore);
+    dealerScore += deck.dealCard().getCardValue(dealerScore);
     
     // Deal two cards to the player, print them and update the  player's scores
     std::cout << "\tPlayer: ";
-    printCard (*cardPtr); // #1
-    playerScore += getCardValue (*cardPtr++, playerScore);
+    playerScore += deck.dealCard().getCardValue(playerScore);
     std::cout << ' ';
-    printCard (*cardPtr); // #2
-    playerScore += getCardValue (*cardPtr++, playerScore);
+    playerScore += deck.dealCard().getCardValue(playerScore); // #2
     
     // Print the current scores
-    printScores (dealerScore, playerScore);
+    printScores(dealerScore, playerScore);
 
     // Store the player's choice
     char playerChoice;
@@ -236,20 +278,17 @@ bool playBlackjack (std::array<Card, 52> &shuffledDeck)
         {
             std::cout << "Enter your move: Hit(H) or Stand(S)? ";
             std::cin >> playerChoice;
-            playerChoice = toupper (playerChoice);
+            playerChoice = toupper(playerChoice);
         } while (playerChoice != 'H' && playerChoice != 'S');
 
         if (playerChoice == 'H')
         {
-            // Player hits, draw another card from the deck and print it
+            // Player hits, draw another card from the deck and update score
             std::cout << "\nDrawn: ";
-            printCard (*cardPtr);
-
-            // Update score and move cardPtr to the next card
-            playerScore += getCardValue (*cardPtr++, playerScore);
+            playerScore += deck.dealCard().getCardValue(playerScore);
             
             // Print the current scores
-            printScores (dealerScore, playerScore);
+            printScores(dealerScore, playerScore);
         }
 
         // If the player goes over 21, busted
@@ -267,14 +306,11 @@ bool playBlackjack (std::array<Card, 52> &shuffledDeck)
     while (dealerScore < 17)
     {
         std::cout << "\nDrawn ";
-        printCard (*cardPtr);
-
-        // Update dealer's score
-        dealerScore += getCardValue (*cardPtr++, dealerScore);
+        dealerScore += deck.dealCard().getCardValue(dealerScore);
    }
 
     // Print the scores after the dealer's turn
-    printScores (dealerScore, playerScore);
+    printScores(dealerScore, playerScore);
     
     // If the dealer goes over 21, dealer busts
     if (dealerScore > 21)
@@ -283,7 +319,7 @@ bool playBlackjack (std::array<Card, 52> &shuffledDeck)
         return true;
     }
 
-    // Control reaches here if both player and dealer stand below 21
+    // Control reaches here only if both player and dealer stand below 21
     // Then, return true if playerScore >= dealerScore, false otherwise
     return (playerScore >= dealerScore);
 }
@@ -291,12 +327,11 @@ bool playBlackjack (std::array<Card, 52> &shuffledDeck)
 // Main function
 int main()
 {
-    // Create an empty deck and populate it with unshuffled cards
-    std::array<Card, 52> deck {};
-    initDeck (deck);
-
     // Seed srand() with system time for calling rand()
-    srand (static_cast<unsigned int> (time(0)));
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Create an empty deck and populate it with unshuffled cards
+    Deck deck;
 
     // Play the game as many time as the player wants
     char playAgain;
@@ -310,13 +345,13 @@ int main()
         std::cout << "\n\n* Instructions\n  ------------";
         std::cout << "\n1. Use the keyboard to enter your moves.\
 \n2. Each card is denoted using two letters: one for its rank and one for its\
-\n   suit. e.g. JS denotes the Jack of Spades, 5H denotes the 5 of spades, etc.\
+\n   suit. e.g. JS denotes the Jack of Spades, 5H denotes the 5 of Spades, etc.\
 \n3. You must know the basic rules of Blackjack.\
 \n4. For simplicity, Player wins ties.\n\n\tSo, let's begin!\n";
         
         // Shuffle the deck and play a single game
-        shuffleDeck (deck);
-        bool playerWon = playBlackjack (deck); 
+        deck.shuffleDeck();
+        bool playerWon = playBlackjack(deck); 
 
         // Print result
         if (playerWon)
@@ -330,7 +365,7 @@ int main()
         {
             std::cout << "\nHave time for another game? (Y/N) ";
             std::cin >> playAgain;
-            playAgain = toupper (playAgain);
+            playAgain = toupper(playAgain);
         } while (playAgain != 'Y' && playAgain != 'N');
     
         // Iterate if the player says yes (Y), break out otherwise
@@ -342,3 +377,4 @@ int main()
     // Return 0 to the OS and terminate the program
     return 0;
 }
+
